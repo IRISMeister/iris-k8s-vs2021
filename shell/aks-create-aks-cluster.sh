49bb733cf068992f4c07c620cd0f372d260bba05
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-10-10 12:22:09 +0900

    wip

--- shell/aks-create-aks-cluster.sh
+++ shell/aks-create-aks-cluster.sh
@@ -7,14 +7,12 @@ az group create --name $aksrg --location "japaneast"
 # AKSクラスタをデプロイ
 
 # IKOをここ(nodepool1)で動かしたいので、--nodepool-taints CriticalAddonsOnly=true:NoScheduleを除去
-az aks create -g $aksrg --name $aksname --node-count 1 --generate-ssh-keys --node-vm-size Standard_DS2_v2
+az aks create -g $aksrg --name $aksname --node-count 1 --generate-ssh-keys --node-vm-size Standard_DS2_v5
 
 # AKS クラスターの資格情報を取得
 az aks get-credentials --overwrite-existing --resource-group $aksrg --name $aksname
 
 # For IRIS cluster
-az aks nodepool add --resource-group $aksrg --cluster-name $aksname --mode user --name iris --node-count 2 --zones 1 2 --node-vm-size Standard_D4s_v3 --labels speedtest/node-type=iris
-az aks nodepool add --resource-group $aksrg --cluster-name $aksname --mode user --name arbiter --node-count 1 --zones 3 --node-vm-size Standard_DS2_v2 --labels speedtest/node-type=iris-arbiter
-# UltraSSD_LRS使用時
-#az aks nodepool add --resource-group $aksrg --cluster-name $aksname --mode user --name irisultra --node-count 2 --zones 1 2 --enable-ultra-ssd --node-vm-size Standard_D4s_v3 --labels speedtest/node-type=iris
-#az aks nodepool add --resource-group $aksrg --cluster-name $aksname --mode user --name arbiter --node-count 1 --zones 3 --node-vm-size Standard_DS2_v2 --labels speedtest/node-type=iris-arbiter
+# UltraSSD_LRS使用時は--enable-ultra-ssdを追加すること
+az aks nodepool add --resource-group $aksrg --cluster-name $aksname --mode user --name iris --node-count 2 --zones 1 2 --node-vm-size Standard_D4s_v5 --labels speedtest/node-type=iris
+az aks nodepool add --resource-group $aksrg --cluster-name $aksname --mode user --name arbiter --node-count 1 --zones 3 --node-vm-size Standard_DS2_v5 --labels speedtest/node-type=iris-arbiter
--- speedtest/k8s/aks/start-speedtest.sh
+++ speedtest/k8s/aks/start-speedtest.sh
@@ -2,17 +2,17 @@ source ../utils.sh
 source ../../../shell/envs.sh
 
 # For speedtest
-az aks nodepool add --resource-group $aksrg --cluster-name $aksname --name ui --node-count 1 --labels speedtest/node-type=master-ui 
-az aks nodepool add --resource-group $aksrg --cluster-name $aksname --name ingest --node-count 1 --labels speedtest/node-type=ingest 
-az aks nodepool add --resource-group $aksrg --cluster-name $aksname --name query --node-count 1 --labels speedtest/node-type=query 
+az aks nodepool add --resource-group $aksrg --cluster-name $aksname --name ui --node-count 1 --node-vm-size Standard_DS2_v5 --labels speedtest/node-type=master-ui 
+az aks nodepool add --resource-group $aksrg --cluster-name $aksname --name ingest --node-count 1 --node-vm-size Standard_DS2_v5 --labels speedtest/node-type=ingest 
+az aks nodepool add --resource-group $aksrg --cluster-name $aksname --name query --node-count 1 --node-vm-size Standard_DS2_v5 --labels speedtest/node-type=query 
 
 # speedtest用のcpf
 kubectl create cm iris-cpf-speedtest --from-file ../cpf/data.cpf
 
 # Speedtest用のPV
-#kubectl apply -f ./yaml/storage-class.yaml
-# aksクラスタ作成時に--enable-ultra-ssd 指定が必要
-kubectl apply -f ./yaml/storage-class-ultra.yaml
+kubectl apply -f ./yaml/storage-class.yaml
+# aksクラスタ作成時もしくはnodepool add時に--enable-ultra-ssd 指定が必要
+#kubectl apply -f ./yaml/storage-class-ultra.yaml
 
 kubectl apply -f ../yaml/deployment-master.yaml
 exit_if_error "Could not deploy master"
